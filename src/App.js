dcc6d3d5202943cb52ff9492f48ae1893156331c
dmitryucsal <dmitry.silva@ucsal.edu.br>
2022-01-09 18:45:23 -0300

    First buttons configurations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Calculator from './components/calculator/Calculator';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Calculator /> 
     </div>
   );
 }
---
+++ src/components/calculator/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { btns, BTN_ACTIONS } from './btnconfig';
+import './calculator.css';
+
+const Calculator = () => {
+    return (
+        <div className='calculator'>
+            <div className="calculator__result"></div>
+        </div>
+    )
+}
+
+export default Calculator
